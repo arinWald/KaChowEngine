15eeb12303d22f7ae12d3a7a00a6fc6de9efd7fb
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-09-28 19:46:21 +0200

    finished About and config windows

--- KaChowEngine/ModuleEditor.cpp
+++ KaChowEngine/ModuleEditor.cpp
@@ -129,6 +129,48 @@ void ModuleEditor::DrawEditor()
 
     }
 
+    if (ImGui::Begin("Help"))
+    {
+        ImGuiIO& io = ImGui::GetIO();
+
+        int cvCounter = 0;
+
+        ImGui::SeparatorText("ABOUT THIS ENGINE:");
+        ImGui::Text("KaChow Engine");
+        ImGui::Text("I'm faster than fast. Faster than fast. I'm lightning!");
+        ImGui::Text("");
+
+        ImGui::Text("By Arnau Gonzalez & Pau Argiz");
+        ImGui::SeparatorText("3rd Party Libraries used");
+        ImGui::BulletText("Glew 20.0");
+        ImGui::BulletText("ImGul 1.51");
+        ImGui::BulletText("MathGeoLib 1.5");
+        ImGui::BulletText("OpenGL 3.1");
+        ImGui::BulletText("OpenGL 3.1");
+
+        ImGui::SeparatorText("LICENSE");
+        ImGui::BulletText("MIT License");
+        ImGui::Text(" ");
+        ImGui::Text("Copyright (c) Arnau Gonzalez & Pau Argiz");
+        ImGui::Text(" ");
+        ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:");
+        ImGui::Text(" ");
+        ImGui::Text("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software");
+        ImGui::Text(" ");
+        ImGui::Text("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY. FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE");
+        ImGui::Text(" ");
+        ImGui::Text("This is the MIT License, which is a very permissive license that allows users to do almost anything they want with the software, including using it for commercial purposes. The only requirement is that the copyright notice and permission notice must be included in all copies or substantial portions of the software.");
+
+        if (ImGui::Button("Add 1 HP to the engine"))
+        {
+            cvCounter++;
+            ImGui::Text("Horse Power: &d", cvCounter);
+        }
+
+        ImGui::End();
+
+    }
+
     // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
     ImGui::ShowDemoWindow();
 
