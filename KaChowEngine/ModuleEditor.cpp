101de8734098adbbbc8f9df5557b1c025fe48326
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-09-30 11:58:29 +0200

    Update ModuleEditor.cpp

--- KaChowEngine/ModuleEditor.cpp
+++ KaChowEngine/ModuleEditor.cpp
@@ -78,44 +78,6 @@ void ModuleEditor::DrawEditor()
             ImGui::Text("Hello world!");
             ImGui::EndMenu();
         }
-        if (ImGui::BeginMenu("About"))
-        {
-            ImGuiIO& io = ImGui::GetIO();
-
-            ImGui::SeparatorText("ABOUT THIS ENGINE:");
-            ImGui::Text("KaChow Engine");
-            ImGui::Text("I'm faster than fast. Faster than fast. I'm lightning!");
-            ImGui::NewLine();
-
-            ImGui::Text("By Arnau Gonzalez & Pau Argiz");
-            ImGui::SeparatorText("3rd Party Libraries used");
-            ImGui::BulletText("Glew 20.0");
-            ImGui::BulletText("ImGui 1.51");
-            ImGui::BulletText("MathGeoLib 1.5");
-            ImGui::BulletText("OpenGL 3.1");
-
-            ImGui::SeparatorText("LICENSE");
-            ImGui::BulletText("MIT License");
-            ImGui::NewLine();
-            ImGui::Text("Copyright (c) Arnau Gonzalez & Pau Argiz");
-            ImGui::NewLine();
-            ImGui::TextWrapped("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:");
-            ImGui::NewLine();
-            ImGui::TextWrapped("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software");
-            ImGui::NewLine();
-            ImGui::TextWrapped("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY. FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE");
-            ImGui::NewLine();
-            ImGui::TextWrapped("This is the MIT License, which is a very permissive license that allows users to do almost anything they want with the software, including using it for commercial purposes. The only requirement is that the copyright notice and permission notice must be included in all copies or substantial portions of the software.");
-
-            if (ImGui::Button("Add 1 HP to the engine"))
-            {
-                cvCounter++;
-            }
-            ImGui::Text("Horse Power: %d", cvCounter);
-
-            ImGui::End();
-        }
-
         if (ImGui::BeginMenu("Configuration"))
         {
             ImGuiIO& io = ImGui::GetIO();
@@ -159,6 +121,45 @@ void ModuleEditor::DrawEditor()
 
             ImGui::End();
         }
+
+        if (ImGui::BeginMenu("About"))
+        {
+            ImGuiIO& io = ImGui::GetIO();
+
+            ImGui::SeparatorText("ABOUT THIS ENGINE:");
+            ImGui::Text("KaChow Engine");
+            ImGui::Text("I'm faster than fast. Faster than fast. I'm lightning!");
+            ImGui::NewLine();
+
+            ImGui::Text("By Arnau Gonzalez & Pau Argiz");
+            ImGui::SeparatorText("3rd Party Libraries used");
+            ImGui::BulletText("Glew 20.0");
+            ImGui::BulletText("ImGui 1.51");
+            ImGui::BulletText("MathGeoLib 1.5");
+            ImGui::BulletText("OpenGL 3.1");
+
+            ImGui::SeparatorText("LICENSE");
+            ImGui::BulletText("MIT License");
+            ImGui::NewLine();
+            ImGui::Text("Copyright (c) Arnau Gonzalez & Pau Argiz");
+            ImGui::NewLine();
+            ImGui::TextWrapped("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:");
+            ImGui::NewLine();
+            ImGui::TextWrapped("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software");
+            ImGui::NewLine();
+            ImGui::TextWrapped("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY. FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE");
+            ImGui::NewLine();
+            ImGui::TextWrapped("This is the MIT License, which is a very permissive license that allows users to do almost anything they want with the software, including using it for commercial purposes. The only requirement is that the copyright notice and permission notice must be included in all copies or substantial portions of the software.");
+
+            if (ImGui::Button("Add 1 HP to the engine"))
+            {
+                cvCounter++;
+            }
+            ImGui::Text("Horse Power: %d", cvCounter);
+
+            ImGui::End();
+        }
+
 
         ImGui::EndMainMenuBar();
     }
