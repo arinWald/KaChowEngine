89bf8d32a1975d9f46bd98ff2eb34704c096a6cc
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-11-01 19:15:20 +0100

    small fix ourPrimitives

--- KaChowEngine/OurPrimitive.cpp
+++ KaChowEngine/OurPrimitive.cpp
@@ -13,73 +13,74 @@ GameObject* OurPrimitive::CreatePrimitive(ShapeType type)
         emptyGameObject->type = ShapeType::EMPTY;
 		return emptyGameObject;
 	}
-
-	// Not Empty
-	GameObject* gameObject = new GameObject();
-    switch (type)
+    else
     {
-    case ShapeType::PLANE:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/plane.fbx");
-        gameObject->name = "Plane";
-        gameObject->type = ShapeType::PLANE;
-        return gameObject;
-        break;
+        // Not Empty
+        GameObject* gameObject = new GameObject();
+        switch (type)
+        {
+        case ShapeType::PLANE:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/plane.fbx");
+            gameObject->name = "Plane";
+            gameObject->type = ShapeType::PLANE;
+            return gameObject;
+            break;
 
-    case ShapeType::CUBE:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/cube.fbx");
-        gameObject->name = "Cube";
-        gameObject->type = ShapeType::CUBE;
-        return gameObject;
-        break;
+        case ShapeType::CUBE:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/cube.fbx");
+            gameObject->name = "Cube";
+            gameObject->type = ShapeType::CUBE;
+            return gameObject;
+            break;
 
-    case ShapeType::SPHERE:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/sphere.fbx");
-        gameObject->name = "Sphere";
-        gameObject->type = ShapeType::SPHERE;
-        return gameObject;
-        break;
+        case ShapeType::SPHERE:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/sphere.fbx");
+            gameObject->name = "Sphere";
+            gameObject->type = ShapeType::SPHERE;
+            return gameObject;
+            break;
 
-    case ShapeType::CYLINDER:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/cylinder.fbx");
-        gameObject->name = "Cylinder";
-        gameObject->type = ShapeType::CYLINDER;
-        return gameObject;
-        break;
+        case ShapeType::CYLINDER:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/cylinder.fbx");
+            gameObject->name = "Cylinder";
+            gameObject->type = ShapeType::CYLINDER;
+            return gameObject;
+            break;
 
-    case ShapeType::MONKEY:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/monkey.fbx");
-        gameObject->name = "Monkey";
-        gameObject->type = ShapeType::MONKEY;
-        return gameObject;
-        break;
+        case ShapeType::MONKEY:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/monkey.fbx");
+            gameObject->name = "Monkey";
+            gameObject->type = ShapeType::MONKEY;
+            return gameObject;
+            break;
 
-    case ShapeType::ICOSHPERE:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/icosphere.fbx");
-        gameObject->name = "Icosphere";
-        gameObject->type = ShapeType::ICOSHPERE;
-        return gameObject;
-        break;
+        case ShapeType::ICOSHPERE:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/icosphere.fbx");
+            gameObject->name = "Icosphere";
+            gameObject->type = ShapeType::ICOSHPERE;
+            return gameObject;
+            break;
 
-    case ShapeType::CONE:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/cone.fbx");
-        gameObject->name = "Cone";
-        gameObject->type = ShapeType::CONE;
-        return gameObject;
-        break;
+        case ShapeType::CONE:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/cone.fbx");
+            gameObject->name = "Cone";
+            gameObject->type = ShapeType::CONE;
+            return gameObject;
+            break;
 
-    case ShapeType::TORUS:
-        gameObject = App->geoLoader->LoadFile("Assets/Models/torus.fbx");
-        gameObject->name = "Torus";
-        gameObject->type = ShapeType::TORUS;
-        return gameObject;
-        break;
+        case ShapeType::TORUS:
+            gameObject = App->geoLoader->LoadFile("Assets/Models/torus.fbx");
+            gameObject->name = "Torus";
+            gameObject->type = ShapeType::TORUS;
+            return gameObject;
+            break;
 
-    default:
-        // Handle the case when an unsupported shape type is provided.
-        // You can throw an exception, log an error, or return a default object.
-        // Here, we'll return nullptr to indicate an error.
-        return nullptr;
-        break;
+        default:
+            // Handle the case when an unsupported shape type is provided.
+            // You can throw an exception, log an error, or return a default object.
+            // Here, we'll return nullptr to indicate an error.
+            return nullptr;
+            break;
+        }
     }
-
 }
